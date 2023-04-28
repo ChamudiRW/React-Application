b8d68fc02bc95f188945d727d7155d30fe97a6b1
ChamudiRW <chamudirw1802@gmail.com>
2023-04-28 14:24:48 +0530

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './styles/App.css';
+import Navbar from './components/nav';
+import Hero from './components/heropage';
+import Footer from './components/footer';
+import Card1 from './components/Cards/card1';
+import Card2 from './components/Cards/card2';
+import Card3 from './components/Cards/card3';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <Navbar />
+      <div className="content">
+        <Hero />
+
+        <Card1 />
+        <Card2 />
+        <Card3 />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }

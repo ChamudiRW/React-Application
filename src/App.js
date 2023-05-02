d48b77c97f4df4f62d045acf50cbd0a532fd8de4
ChamudiRW <chamudirw1802@gmail.com>
2023-05-02 17:21:01 +0530

    app.js with the edits

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/nav';
 import Hero from './components/heropage';
 import Footer from './components/footer';
 import Cards from './components/Cards/cards';
+import Aboutus from './components/aboutus';
+import How from './components/howitworks';
+import Gallery from './components/gallery';
 
 function App() {
   return (
@@ -12,6 +15,14 @@ function App() {
         <Hero />
         <br />
         <Cards />
+        <br />
+        <Aboutus />
+        <br />
+        <How />
+        <Gallery />
+        <br /> <br />
+        <br /> <br /> <br /> <br />
+        <br /> <br />
       </div>
       <div className="footer">
         <Footer />

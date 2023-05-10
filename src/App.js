71dd8927d3e0bc33dbf217e621793c86c2b946d3
ChamudiRW <chamudirw1802@gmail.com>
2023-05-11 00:04:16 +0530

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Navbar from './components/navbar';
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <Navbar />
+      </header>
       <div className="content"></div>
       <div className="footer"></div>
     </div>
